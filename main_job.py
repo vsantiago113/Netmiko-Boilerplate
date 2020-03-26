0a8c13f2ffa4e4b4bf6edde1ff0e7581ed6e6932
vsantiago113sec@gmail.com <Vanillaface10%>
2020-03-26 10:00:43 -0500

    Update

---
+++ main_job.py
@@ -0,0 +1,32 @@
+from netmiko import ConnectHandler
+from multiprocessing import Pool, Manager
+import tqdm
+
+
+def run_job(args):
+    device, dictionary = args
+
+    connection = ConnectHandler(**device)
+
+    output = connection.send_command('show ip int b')
+    dictionary[device['host']] = {'results': [output]}
+
+    connection.disconnect()
+
+
+if __name__ == '__main__':
+    devices = [{'device_type': 'cisco_ios', 'host': '192.168.1.201', 'username': 'admin', 'password': 'Cisco123'},
+               {'device_type': 'cisco_ios', 'host': '192.168.1.202', 'username': 'admin', 'password': 'Cisco123'},
+               {'device_type': 'cisco_ios', 'host': '192.168.1.203', 'username': 'admin', 'password': 'Cisco123'}]
+    manager = Manager()
+    shared_dictionary = manager.dict()
+    devices = [(i, shared_dictionary) for i in devices]
+    pool = Pool(processes=7)
+    for _ in tqdm.tqdm(pool.imap_unordered(run_job, devices), total=len(devices)):
+        pass
+
+    for k, v in shared_dictionary.items():
+        print(f'Host: {k}')
+        for o in v['results']:
+            print(o)
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
